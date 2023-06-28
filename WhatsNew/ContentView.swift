c9404c668ced44bababc30bf4119bf541a6db965
Derick <21DerickDerick@gmail.com>
2023-06-29 05:25:07 +0800

    Initial Commit

---
+++ WhatsNew/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatsNew
+//
+//  Created by Derick on 29/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatsNew/WhatsNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsNewApp.swift
+//  WhatsNew
+//
+//  Created by Derick on 29/6/23.
+//
+
+import SwiftUI
+
+@main
+struct WhatsNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
